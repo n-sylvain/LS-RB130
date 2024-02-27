cb18e68a8072dabea45811fd33134b766c5f85da
Sylvain Ni <sylvainni@msn.com>
2024-02-27 17:14:16 +0100

    Todo update

--- lesson1/todo/todo_extended_select.rb
+++ lesson1/todo/todo_extended_select.rb
@@ -127,6 +127,30 @@ class TodoList
     end
     list
   end
+
+  def find_by_title(title)
+    select { |todo| todo.title == title }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
 end
 
 todo1 = Todo.new("Buy milk")

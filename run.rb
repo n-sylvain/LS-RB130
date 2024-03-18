106750db50e23daa6cdc534f2699b3a2bc1f4779
Sylvain Ni <sylvainni@msn.com>
2024-03-18 18:20:06 +0100

    Custom reduce example

--- run.rb
+++ run.rb
@@ -1,12 +1,27 @@
-def count(array)
-  total = 0
-  array.each { |value| total += 1 if yield value }
-  total
+def custom_reduce(array, default=nil)
+  counter = default.nil? ? 1 : 0
+  accumulator = default.nil? ? array[0] : default
+
+  while counter < array.size
+    # p [counter, accumulator, array[counter]]
+
+    result = yield(accumulator, array[counter])
+    if result.nil?
+      raise StandardError, 'Block returned nil'
+    else
+      accumulator = result
+    end
+    counter += 1
+  end
+
+  accumulator
 end
 
-p count([1,2,3,4,5]) { |value| value.odd? } == 3
-p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
-p count([1,2,3,4,5]) { |value| true } == 5
-p count([1,2,3,4,5]) { |value| false } == 0
-p count([]) { |value| value.even? } == 0
-p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
+
+array = [1, 2, 3, 4, 5]
+
+p custom_reduce(array) { |acc, num| acc + num }                    # => 15
+p custom_reduce(array, 10) { |acc, num| acc + num }                # => 25
+# p custom_reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p custom_reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
+p custom_reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']

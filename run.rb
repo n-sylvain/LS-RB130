526152ca93d1cfe5c7ab98a99c0fb505b6045f4c
Sylvain Ni <sylvainni@msn.com>
2024-03-19 18:45:29 +0100

    Custom reduce example

--- run.rb
+++ run.rb
@@ -22,6 +22,6 @@ array = [1, 2, 3, 4, 5]
 
 p custom_reduce(array) { |acc, num| acc + num }                    # => 15
 p custom_reduce(array, 10) { |acc, num| acc + num }                # => 25
-# p custom_reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p custom_reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
 p custom_reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
 p custom_reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']

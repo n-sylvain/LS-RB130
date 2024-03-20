ef3f697dc236445a0602f1642c412fec1d29062c
Sylvain Ni <sylvainni@msn.com>
2024-03-20 17:50:48 +0100

    2nd pass files

--- run.rb
+++ run.rb
@@ -1,27 +1,16 @@
-def custom_reduce(array, default=nil)
-  counter = default.nil? ? 1 : 0
-  accumulator = default.nil? ? array[0] : default
-
-  while counter < array.size
-    # p [counter, accumulator, array[counter]]
-
-    result = yield(accumulator, array[counter])
-    if result.nil?
-      raise StandardError, 'Block returned nil'
-    else
-      accumulator = result
-    end
-    counter += 1
-  end
-
-  accumulator
+def proc_to_block(proc)
+  p proc.class
+  p proc
+  p [1, 2, 3, 4, 5].any?(proc) # => #any? expects a block.
 end
 
+def block_to_proc(&block)
+  yield # => the block is still there to be yielded to
+  block.call # => but the unary & operator has converted the block into a proc object also
+end
 
-array = [1, 2, 3, 4, 5]
-
-p custom_reduce(array) { |acc, num| acc + num }                    # => 15
-p custom_reduce(array, 10) { |acc, num| acc + num }                # => 25
-p custom_reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
-p custom_reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
-p custom_reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
+block_to_proc { puts "from a block"} # => from a block
+                                     # => from a block
+my_proc = proc {|n| n > 4}
+proc_to_block(my_proc) # => Proc
+                       # => true
